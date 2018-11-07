542bae7f16f285c643aa89c563de2a36eb224296
Donnie Keathley <dkeathley@gmail.com>
2018-11-07 16:20:52 -0500

    Updates to Code
    Improved settings and updated syntax to make it more uniform.
    
    Now you can specify the angle of the incoming wave as well as the
    material absorption coefficient (through the conductivity of the
    material).  This all scales as it should for variable resolution.

--- nano_tip_sim.py
+++ nano_tip_sim.py
@@ -6,19 +6,25 @@ import numpy as np
 #----------------
 # Settings
 #----------------
-tip_radius = 0.0075
+
+#Save Settings
+save_prefix = "t_shift_175mrad_r1000_7nmrad"
+
+#Tip Properties
+tip_radius = 0.007
 cone_height = 0.364
 trunk_radius = 0.275
 
-X_1 = -tip_radius*1.1
-X_2 = tip_radius*1.1
-Y_1 = -tip_radius*1.1
-Y_2 = tip_radius*1.1
-Z_1 = -tip_radius*1.1
-Z_2 = tip_radius*1.1
+#Variable resolution properties
+X_1 = -1.1*tip_radius
+X_2 = 1.1*tip_radius
+Y_1 = -2*tip_radius
+Y_2 = 2*tip_radius
+Z_1 = -1.1*tip_radius
+Z_2 = 1.1*tip_radius
 
 sx = 0.8;
-sy = 2.0;
+sy = 1.5;
 sz = 1.0;
 dpml = 0.1;
 
@@ -27,14 +33,31 @@ fcen = 1.25;
 res = 1000;
 res_factor = 0.2;
 waves = 7.5;
-theta = 0;
+theta = 0.175;
 
 #Tip Properties
-eps_tip = 3.694;
+n_tip = 3.694
+#n_tip = 1.0
+k_tip = 0.0
+#k_tip = 0.0065435
+
 
 #---------------------
 # Calcs
 #---------------------
+
+#Functions for calculating index of refraction -- meep style
+def calc_sig_d(n, k, fcen):
+    eps = (n + 1j*k)**2
+    eps_r = np.real(eps)
+    return 2*np.pi*fcen*np.imag(eps)/eps_r
+
+def calc_eps_r(n, k):
+    eps = (n + 1j*k)**2
+    return np.real(eps)
+
+eps_tip = calc_eps_r(n_tip, k_tip)
+sig_d_tip = calc_sig_d(n_tip, k_tip, fcen)
 
 #Create the cell size
 sX = 2*dpml + sx;
@@ -55,15 +78,17 @@ cell = mp.Vector3(sX_prime, sY_prime, sZ_prime)
 
 
 tip = [mp.Cylinder(radius=trunk_radius,
-                   center=mp.Vector3(0.0, -1*sY/4.0 - cone_height/2.0, 0.0),
+                   center=mp.Vector3(0.0,
+                                     -1*sY/4.0 - cone_height/2.0 - tip_radius,
+                                     0.0),
                    height=(sY/2.0 - cone_height),
                    axis=mp.Vector3(0.0, 1.0, 0.0)),
-       mp.Cone(center=mp.Vector3(0, -1*cone_height/2.0),
+       mp.Cone(center=mp.Vector3(0, -1*cone_height/2 - tip_radius, 0),
                height=cone_height,
                radius=trunk_radius,
                radius2=tip_radius,
                axis=mp.Vector3(0,1,0)),
-       mp.Sphere(center=mp.Vector3(0,0,0),
+       mp.Sphere(center=mp.Vector3(0,-1*tip_radius,0),
                  radius=tip_radius)]
 
 
@@ -117,8 +142,13 @@ def mat_func(r):
     eps_diag = eps_transform.diagonal()
     mu_diag = mu_transform.diagonal()
     
-    mat = mp.Medium(epsilon_diag = mp.Vector3(eps_diag[0,0], eps_diag[0,1], eps_diag[0,2]),
-                           mu_diag=mp.Vector3(mu_diag[0,0], mu_diag[0,1], mu_diag[0,2]))
+    mat = mp.Medium(epsilon_diag = mp.Vector3(eps_diag[0,0],
+                                              eps_diag[0,1],
+                                              eps_diag[0,2]),
+                    mu_diag=mp.Vector3(mu_diag[0,0],
+                                       mu_diag[0,1],
+                                       mu_diag[0,2]),
+                    D_conductivity=sig_d_tip)
 
 
     return mat
@@ -126,17 +156,23 @@ def mat_func(r):
 
 #Create source amplitude function:
 ky = fcen*np.sin(theta)
+ky_prime = ky*sY/sY_prime
 def my_amp_func_y(r):
 
+    r_prime_y = r.y
+    y_fac = 1
+    
     if((r.x >= X_1)and(r.x <= X_2)):
         x_fac = 1
     else:
         x_fac = res_factor
 
-    if((r.y >= Y_1)and(r.y <= Y_2)):
-        y_fac = 1
-    else:
+    if(r.y < Y_1):
         y_fac = res_factor
+        r_prime_y = Y_1 + (r.y - Y_1)/res_factor
+    elif(r.y > Y_2):
+        y_fac = res_factor
+        r_prime_y = Y_2 + (r.y - Y_2)/res_factor
 
     if((r.z >= Z_1)and(r.z <= Z_2)):
         z_fac = 1
@@ -147,22 +183,28 @@ def my_amp_func_y(r):
 
     transform = J/np.linalg.det(J);
     
-    phase_factor = np.exp(-1*2*1j*np.pi*ky*r.y)
+    phase_factor = np.exp(-1*2*1j*np.pi*ky*r_prime_y)
     amp_factor = transform.diagonal()[0, 1]
     
     return amp_factor*phase_factor
 
 def my_amp_func_z(r):
 
+    r_prime_y = r.y
+    y_fac = 1
+
     if((r.x >= X_1)and(r.x <= X_2)):
         x_fac = 1
     else:
         x_fac = res_factor
 
-    if((r.y >= Y_1)and(r.y <= Y_2)):
-        y_fac = 1
-    else:
+    if(r.y < Y_1):
         y_fac = res_factor
+        r_prime_y = Y_1 + (r.y - Y_1)/res_factor
+    elif(r.y > Y_2):
+        y_fac = res_factor
+        r_prime_y = Y_2 + (r.y - Y_2)/res_factor
+        
 
     if((r.z >= Z_1)and(r.z <= Z_2)):
         z_fac = 1
@@ -173,21 +215,12 @@ def my_amp_func_z(r):
 
     transform = J/np.linalg.det(J);
     
-    phase_factor = np.exp(-1*2*1j*np.pi*ky*r.y)
+    phase_factor = np.exp(-1*2*1j*np.pi*ky*r_prime_y)
     amp_factor = transform.diagonal()[0, 2]
     
     return amp_factor*phase_factor
 
-# #Functions for calculating index of refraction -- meep style
-# def calc_sig_d(n, k, fcen):
-#     eps = (n + 1j*k)**2
-#     eps_r = np.real(eps)
 
-#     return 2*np.pi*fcen*np.imag(eps)/eps_r
-
-# def calc_eps_r(n, k):
-#     eps = (n + 1j*k)**2
-#     return np.real(eps)
 
 
 #Create PMLs
@@ -226,14 +259,16 @@ sim = mp.Simulation(cell_size=cell,
                     resolution=res,
                     symmetries=symmetry,
                     dimensions=3,
-                    k_point=mp.Vector3(0, ky, 0),
+                    k_point=mp.Vector3(0, -1*ky_prime, 0),
                     material_function=mat_func,
-                    extra_materials=[mp.Medium(epsilon=eps_tip,mu=4)],
+                    extra_materials=[mp.Medium(epsilon=eps_tip,
+                                               mu=4,
+                                               D_conductivity=1)],
                     verbose=True)
 
 
-sim.run(mp.in_volume(monitor_xy, mp.to_appended("tEy_xy", mp.at_every(1/fcen/sample, mp.output_efield_y))),
-        mp.in_volume(monitor_yz, mp.to_appended("tEy_yz", mp.at_every(1/fcen/sample, mp.output_efield_y))), until=waves/fcen)
+sim.run(mp.in_volume(monitor_xy, mp.to_appended(save_prefix + "Ey_xy", mp.at_every(1/fcen/sample, mp.output_efield_y))),
+        mp.in_volume(monitor_yz, mp.to_appended(save_prefix + "Ey_yz", mp.at_every(1/fcen/sample, mp.output_efield_y))), until=waves/fcen)
 
 
 
